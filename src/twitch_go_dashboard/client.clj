ad187fa0be6037429f20d2dbcfb1efaf52497446
Almantas Stanžys <a.stanzys@gmail.com>
2015-05-25 23:45:07 +0300

    get broadcasts

--- src/twitch_go_dashboard/client.clj
+++ src/twitch_go_dashboard/client.clj
@@ -2,12 +2,12 @@
   (:require [clojure.data.json :as json]
             [org.httpkit.client :as http]))
 
-(def headers
-  {:Accept "application/vnd.twitchtv.v3+json"})
+(def twitch-headers
+  {"Accept" "application/vnd.twitchtv.v3+json"})
 
-(defn do-get [url]
+(defn do-get [url options]
   (-> url
-      (http/get headers)
+      (http/get options)
       deref
       :body
       (json/read-str :key-fn keyword)))
@@ -17,4 +17,9 @@
 
 (defn fetch-channel [name]
   (-> (format-url "https://api.twitch.tv/kraken/channels/%s" name)
-      do-get))
+      (do-get {:headers twitch-headers})))
+
+(defn fetch-channel-broadcasts [name]
+  (-> (format-url "https://api.twitch.tv/kraken/channels/%s/videos" name)
+      (do-get {:headers twitch-headers
+               :query-params {:broadcasts "true"}})))
