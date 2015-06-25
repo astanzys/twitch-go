2572750b96c410aca9969441ba8c4c8d301dee83
Almantas Stanžys <a.stanzys@gmail.com>
2015-06-25 23:13:47 +0300

    base routes

--- src/twitch_go_dashboard/handler.clj
+++ src/twitch_go_dashboard/handler.clj
@@ -1,11 +1,14 @@
 (ns twitch-go-dashboard.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.util.response :refer [redirect]]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
-  (route/not-found "Not Found"))
+  (GET "/" [] (redirect "index.html"))
+  (route/resources "/")
+  (route/not-found "<h1>Page not found</h1>"))
+
 
 (def app
   (wrap-defaults app-routes site-defaults))
